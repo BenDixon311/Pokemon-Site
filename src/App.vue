2ac721e8b4937e898e7a4611197d1c0391622368
Ben Dixon <bendixon311@gmail.com>
2018-11-07 21:16:14 -0700

    Update src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,73 @@
 <template>
-    <div class="container">
-
-        <app-movie></app-movie>
-
-
-        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-        <app-quote-grid :quotes="quotes"></app-quote-grid>
-    </div>
+  <v-app id="inspire" dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <v-flex shrink>
+            <v-tooltip right>
+              <v-btn
+                slot="activator"
+                :href="source"
+                icon
+                large
+                target="_blank"
+              >
+                <v-icon large>code</v-icon>
+              </v-btn>
+              <span>Source</span>
+            </v-tooltip>
+            <v-tooltip right>
+              <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/qxQWda" target="_blank">
+                <v-icon large>mdi-codepen</v-icon>
+              </v-btn>
+              <span>Codepen</span>
+            </v-tooltip>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer app fixed>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import QuoteGrid from './components/QuoteGrid.vue';
-import NewQuote from './components/NewQuote.vue';
-import Movie from './components/Movie.vue';
-    export default {
-        data: function() {
-            return {
-                quotes: [
-                    'test quote'
-                ],
-                maxQuotes: 10
-            }
-        },
-        components: {
-          appQuoteGrid: QuoteGrid,
-          appNewQuote: NewQuote,
-          appMovie: Movie
-        },
-        methods: {
-          newQuote(quote) {
-            this.quotes.push(quote);
-          }
-        }
+  export default {
+    data: () => ({
+      drawer: null
+    }),
+    props: {
+      source: String
     }
+  }
 </script>
-
-<style>
-</style>
