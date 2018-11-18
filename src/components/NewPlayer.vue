4628564edfe3dd5c994b58f8bcfb7b2d5cfd5c66
Ben Dixon <bendixon311@gmail.com>
2018-11-17 21:25:44 -0700

    Added NewPlayer and Roster
    Still need to add to main App component

---
+++ src/components/NewPlayer.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field
+                v-model="name"
+                :rules="nameRules"
+                :counter="20"
+                label="Name"
+                required
+                
+            ></v-text-field>
+            <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                label="Email"
+                required
+                
+            ></v-text-field>
+             <v-text-field
+                v-model="height"
+                label="Height"
+                required
+                
+            ></v-text-field>
+             <v-text-field
+                v-model="weight"
+                label="Weight"
+                required
+                
+            ></v-text-field>
+            <v-select
+                v-model="position"
+                :items="positions"
+                :rules="positionRules"
+                label="Position"
+                required
+                
+            ></v-select>
+
+        </v-form>
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    data () {
+
+        return {
+            playerData: {
+                name: '',
+                nameRules: [
+                     v => !!v || 'Name is required',
+                     v => (v && v.length <= 20) || 'Name must be less than 20 characters'
+                    ],
+                email: '',
+                emailRules: [
+                     v => !!v || 'E-mail is required',
+                     v => /.+@.+/.test(v) || 'E-mail must be valid'
+                     ],
+                height: '',
+                weight: '',
+                position: null,
+                positions: [
+                    'Shooting Guard',
+                    'Point Guard',
+                    'Power Forward',
+                    'Small Forward',
+                    'Center'
+                ],
+                positionRules: [v => !!v || 'Select Position']
+
+            }
+        }
+    }
+}
+</script>
+
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import NBA from 'nba'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
+
 Vue.use(VueResource);
 Vue.use(NBA);
 Vue.config.productionTip = false
